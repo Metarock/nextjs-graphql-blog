55cb4134e43fa1b1ec70638d5c033c667a72c878
Metarock <esang037@gmail.com>
2022-03-30 16:34:53 +1300

    converted project from ts to js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
