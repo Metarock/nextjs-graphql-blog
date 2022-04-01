e9810bdf2d47706987ab2fa1adee87484a4d0076
Metarock <esang037@gmail.com>
2022-04-01 17:09:44 +1300

    styled the widget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
-const PostWidget = () => {
-  return <div>PostWidget</div>
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    // if there is a slug
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      )
+    } else {
+      // not it in a specific category
+      getRecentPosts().then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+
+  console.log(relatedPosts)
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {/* map all of our posts */}
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="mb-4 flex  w-full items-center">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="rounded-full align-middle"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="ml-4 flex-grow">
+            <p className="font-xs text-gray-500">
+              {moment(post.createdat).format('MMM DD, YYYY')}
+            </p>
+            <Link
+              key={post.title}
+              href={`/post/${post.slug}`}
+              className="text-md"
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default PostWidget
--- services/index.js
+++ services/index.js
@@ -2,7 +2,7 @@ import { request, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHICS_ENDPOINT
 
-export const getPosts = async () => {
+const getPosts = async () => {
   const query = gql`
     query MyQuery {
       postsConnection {
@@ -39,3 +39,56 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges
 }
+
+const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      # get the posts but order them in an ascending order
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        # what do we want to get from the psots
+        title
+        featuredImage {
+          url 
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.posts
+}
+
+const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($categories: [String!], $slug: String!) {
+      posts(
+        # dont display the current article, but display other articles that include other categories that we want to get
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        # what do we want to get from the psots
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.posts
+}
+
+export { getPosts, getRecentPosts, getSimilarPosts }
