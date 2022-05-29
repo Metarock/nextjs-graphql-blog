8b67bf1f195e0b6b4548ebf6f9c904615d30cd87
Metarock <esang037@gmail.com>
2022-05-29 21:01:02 +1200

    add comments

--- services/index.js
+++ services/index.js
@@ -142,6 +142,22 @@ const getCategories = async () => {
   return result.categories
 }
 
+const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.comments
+}
+
 // POST to
 const submitComment = async (obj) => {
   // use nextjs own backend from the folder "api" as the root folder
@@ -162,4 +178,5 @@ export {
   getCategories,
   getPostDetails,
   submitComment,
+  getComments,
 }
